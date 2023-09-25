3ec6e9bd5b992053713b49ad9cbfbcc7d0ab312e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-09-25 18:27:57 +0300

    add div for form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 
 function App() {
-  return <h1>Hi</h1>
+  return (  <div style = {{
+    position: "relative",
+    background: "white",
+    border: "1px solid black",
+    padding: "2rem",
+    margin: "1rem",
+    borderRadius: ".5rem",
+    fontFamily: "Arial"
+  }}>
+    <form action="">
+
+    </form>
+
+  </div>)
 }
 
 export default App
