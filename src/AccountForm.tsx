5e224a8996ce64b67fb726bb5dc6815aa21c4888
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-09-26 19:01:39 +0300

    add style for form input

--- src/AccountForm.tsx
+++ src/AccountForm.tsx
@@ -1,9 +1,11 @@
-export function AccountForm() {
-    return <>
-    <label htmlFor="">Email</label>
-    <input type="email" autoFocus required/>
-    <label htmlFor="">Password</label>
-    <input type="password" required/>
+import { FormWrapper } from "./FormWrapper";
 
-</>
-}
+export function AccountForm() {
+    return (
+    <FormWrapper title= "Account Creating">
+        <label htmlFor="">Email</label>
+        <input type="email" autoFocus required/>
+        <label htmlFor="">Password</label>
+        <input type="password" required/>
+    </FormWrapper>
+)}
--- src/AddressForm.tsx
+++ src/AddressForm.tsx
@@ -1,12 +1,15 @@
+import { FormWrapper } from "./FormWrapper";
+
 export function AddressForm() {
-    return <>
-    <label htmlFor="">Street</label>
-    <input type="text" autoFocus required/>
-    <label htmlFor="">City</label>
-    <input type="text" required/>
-    <label htmlFor="">State</label>
-    <input type="text" required/>
-    <label htmlFor="">Zip</label>
-    <input type="text" required/>
-</>
-}
+    return (
+    <FormWrapper title="Address Details">
+        <label htmlFor="">Street</label>
+        <input type="text" autoFocus required/>
+        <label htmlFor="">City</label>
+        <input type="text" required/>
+        <label htmlFor="">State</label>
+        <input type="text" required/>
+        <label htmlFor="">Zip</label>
+        <input type="text" required/>
+    </FormWrapper>
+)}
